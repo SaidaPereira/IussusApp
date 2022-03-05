307d2829afd184e15d8bf849ef2e12eab451efa6
SaidaPereira <saidamabelpereira2000@gmail.com>
2022-03-05 19:37:35 -0300

    Ajuste de intro y menú principal (esqueleto)

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -7,21 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPage implements OnInit {
 
-  slideOpts = {
-    initialSlide: 0,
-    slidesPerView:1,
-    centeredSlides:true,
-    speed: 400
-  };
-
-  slides=[
-    {
-      imageSrc: "assets/img/IussusApp.png",
-      imageAlt: "IussusAp",
-      title  : "Bienvenido a:",
-      descripcion: ""
-    }
-  ];
+  
 
   constructor() { }
 

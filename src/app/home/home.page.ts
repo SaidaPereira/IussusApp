2dfce1b9a6128a91d5923433636e2ad33072a016
SaidaPereira <saidamabelpereira2000@gmail.com>
2022-03-07 23:29:50 -0300

    Pantalla principal parte visual realizada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,60 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomePage {
   public home: string;
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
     this.home = this.activatedRoute.snapshot.paramMap.get('id');
   }
+  // async presentActionSheet() {
+  //   const actionSheet = await this.actionSheetController.create({
+  //     header: 'Albums',
+  //     cssClass: 'my-custom-class',
+  //     buttons: [{
+  //       text: 'Delete',
+  //       role: 'destructive',
+  //       icon: 'trash',
+  //       id: 'delete-button',
+  //       data: {
+  //         type: 'delete'
+  //       },
+  //       handler: () => {
+  //         console.log('Delete clicked');
+  //       }
+  //     }, {
+  //       text: 'Share',
+  //       icon: 'share',
+  //       data: 10,
+  //       handler: () => {
+  //         console.log('Share clicked');
+  //       }
+  //     }, {
+  //       text: 'Play (open modal)',
+  //       icon: 'caret-forward-circle',
+  //       data: 'Data value',
+  //       handler: () => {
+  //         console.log('Play clicked');
+  //       }
+  //     }, {
+  //       text: 'Favorite',
+  //       icon: 'heart',
+  //       handler: () => {
+  //         console.log('Favorite clicked');
+  //       }
+  //     }, {
+  //       text: 'Cancel',
+  //       icon: 'close',
+  //       role: 'cancel',
+  //       handler: () => {
+  //         console.log('Cancel clicked');
+  //       }
+  //     }]
+  //   });
+  //   await actionSheet.present();
 
+  //   const { role, data } = await actionSheet.onDidDismiss();
+  //   console.log('onDidDismiss resolved with role and data', role, data);
+  // }
+
+  
 }

4255407142b82137701d2d649299c4a4e7a7b0b3
SaidaPereira <saidamabelpereira2000@gmail.com>
2022-03-17 23:28:53 -0300

    Se agrego el abm de estado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,7 +51,11 @@ const routes: Routes = [
   {
     path: 'productos',
     loadChildren: () => import('./productos/productos.module').then( m => m.ProductosPageModule)
+  },  {
+    path: 'estado',
+    loadChildren: () => import('./estado/estado.module').then( m => m.EstadoPageModule)
   }
+
 
 
 ];

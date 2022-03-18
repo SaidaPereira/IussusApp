d49d0bee87918594da7ce0de2563c93d66f46b77
SaidaPereira <42759887+SaidaPereira@users.noreply.github.com>
2022-03-18 00:33:00 -0300

    Revert "Revert "Registroproductos""

---
+++ src/app/registroproducto/registroproducto.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registroproducto',
+  templateUrl: './registroproducto.page.html',
+  styleUrls: ['./registroproducto.page.scss'],
+})
+export class RegistroproductoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

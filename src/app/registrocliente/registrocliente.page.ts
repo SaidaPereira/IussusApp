1887eabe950a5f71a846e3d23cdad4a43c207a97
SaidaPereira <saidamabelpereira2000@gmail.com>
2022-04-04 18:23:33 -0400

    Ajustes varios 2

--- src/app/registrocliente/registrocliente.page.ts
+++ src/app/registrocliente/registrocliente.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClienteService } from '../../services/cliente.service';
 
 @Component({
   selector: 'app-registrocliente',
@@ -7,24 +8,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registrocliente.page.scss'],
 })
 export class RegistroclientePage implements OnInit {
-  public registroCliente : FormGroup;
+  public registrocliente : FormGroup;
 
-  constructor(
-    private formBuilder : FormBuilder
-  ) {
-    this.registroCliente = this.formBuilder.group({
+  constructor( private formBuilder : FormBuilder,private clienteService : ClienteService) { }
+
+  ngOnInit() {
+    this.registrocliente = this.formBuilder.group({
       nombre : ['', Validators.required],
       apellido : ['', Validators.required],
       direccion : ['', Validators.required],
-      
-    })
-   }
-
-  ngOnInit() {
+      ruci: [''],
+      telefono :[''],
+      correo : [],
+      notas : [' ']
+    });
   }
 
+  public enviardata(){
+    this.clienteService.Create({
+      nombre : this.registrocliente.value.nombre,
+      apellido : this.registrocliente.value.apellido,
+      direccion : this.registrocliente.value.direccion,
+      ruci:this.registrocliente.value.ruci,
+      telefono :this.registrocliente.value.telefono,
+      correo : this.registrocliente.value.correo,
+      notas: this.registrocliente.value.notas
+    }).subscribe(respuesta=>{
+      console.log('datos',respuesta);
+    });
 
-  logForm() {
 
   }
+  
 }
